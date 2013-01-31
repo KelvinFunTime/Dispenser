1f77cdcaebe456ca065891c9e1b3e3f0bb93998a
KelvinFunTime <kelvinfuntime@gmail.com>
2013-01-31 13:57:23 -0800

    Updated Pump Code

--- source/PumpControllingSystem.c
+++ source/PumpControllingSystem.c
@@ -100,10 +100,10 @@ int SetCupSize (void)
 		printf ("\nPlease Enter Your Cup Size (in ounces): ");		// Prompts user for cup size in oz
 		scanf ("%f", &cup_size);										// gets user input for cup size
 		
-	}	while (cup_size < 0 && cup_size > 34);						// wont exit until valid cup size
+	}	while (cup_size < 0 || cup_size > 34);						// wont exit until valid cup size
 	
 	cup_size = (cup_size * 1.2);									// 1.2 seconds for each ounce
-	//cup_size = (int)((cup_size * PI_CLOCK)/10.5);	
+		
 										// this gives correct timing per ounce
 	return (cup_size);								// creates correct value for loop
 }																	// according to cup size determined
