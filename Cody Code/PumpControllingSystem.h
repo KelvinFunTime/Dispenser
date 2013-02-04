569ecf5961cde0e16d4c570d6b1b61c7960f65f3
unknown <kelvinfuntime@gmail.com>
2013-02-04 11:05:32 -0800

    Removed Cody code

--- Cody Code/PumpControllingSystem.h
+++
@@ -1,4 +0,0 @@
-extern int PumpSelect (void);
-extern int PumpControl (int pump_select, int time_interval);
-extern int SetCupSize (void);
-extern void initPumpPins (void);
--- Cody Code/PumpSelect.h
+++
@@ -1,3 +0,0 @@
-int PumpSelect (void);
-double SetCupSize ();
-int PumpControl (int pump_select, int time_interval);
