09c0f128536772bcff035eef4d321e77ce47676e
unknown <cody.bulkley@CYCLOPS.students.cset.oit.edu>
2013-02-14 10:14:50 -0800

    Changed PWM Code

--- source/ServoControl.c
+++ source/ServoControl.c
@@ -5,21 +5,21 @@
 
 #define PWN_PIN 12	// gpio 18, pi 12
 #define	PWM_OUTPUT	2
-
+#define SW_PWM 0 //(gpio 0, pin 11)
 int main ()
 {
 	int i = 0;
 	int j = 20;
 	
-	 pinMode(PWM_PIN,PWM_OUTPUT); //setup hardware pwm
-	
+	// pinMode(PWM_PIN,PWM_OUTPUT); //setup hardware pwm
+	softPwmCreate(SW_PWM,0,100); //setup software pwm pin 
 	if ( wiringPiSetup() == -1 )
 		return 1;
 		
 	
 	for (i = 0; i < j; i++)
 	{
-		pwnWrite (PWM_PIN, i);
+		softpwnWrite (SW_PWM, i);
 	}
 	
 	
