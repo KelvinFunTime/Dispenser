e14b5d2d61ad6a75ff480ca4bc25646d9a1c3cd5
Wesley Maxey <wesley.maxey@gmail.com>
2013-02-20 10:27:19 -0800

    Thread update

--- source/PortIO_Thread.h
+++ source/PortIO_Thread.h
@@ -1,14 +1,14 @@
 #ifndef PORTIO
 #define PORTIO
 
-struct port_args
+typedef struct port_args
 {
 	short data;
 	short drink_size;
 	short pump_sel;
 	int deb_length
 	short pin_num;
-};
+} port_args;
 
 extern void service_thread(port_args * Args);
 extern void pump_pwm( short pin_num, short * time, short * pwm, short * data );
