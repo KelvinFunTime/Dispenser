b875565a1c5495d111aed8f0178a4fd7888c297f
KelvinFunTime <kelvinfuntime@gmail.com>
2013-02-23 18:37:09 -0800

    More Servo Stuff

--- source/ServoControl.c
+++ source/ServoControl.c
@@ -4,8 +4,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "ServoControl.h"
-
-#define PWM_PIN 1	// gpio 18, pi 12
+#include "defs.h"
 
 int pwm_control(short pos)
 {
@@ -15,7 +14,7 @@ int pwm_control(short pos)
 	
 	system("gpio pwm-ms");
 	system("gpio pwmc 200");
-	system("gpio pwm 1 150");
+	system("gpio pwm 1 %d", pos);
 
 	return 0;
 }
