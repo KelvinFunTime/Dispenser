68e073873ae83bad503867c0e6fe1b83f50563cf
unknown <cody.bulkley@BEAST.students.cset.oit.edu>
2013-02-23 16:16:06 -0800

    Servo Test Code...

--- source/ServoControl.c
+++ source/ServoControl.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
-#include <string.h>
 #include <wiringPi.h>
 
 #define PWM_PIN 1	// gpio 18, pi 12
 
 int main ()
 {
-	int i = 0;
-	int j = 30;
-
-	if ( wiringPiSetup() == -1 )
+	if ( wiringPiSetup() == -1 )	// sets up the wiring pi
 		return 1;
+		
+	pwmSetClock (200);				// Sets the proper clock divider
 
-	pinMode(PWM_PIN,PWM_OUTPUT); //setup hardware pwm
+	pinMode(PWM_PIN,PWM_OUTPUT); 	//setup hardware pwm
 	
-	for (i = 0; i < j; i++)
-		pwmWrite (PWM_PIN, i);
+	
+	while (1)
+	{
+		pwmWrite (PWM_PIN, 150);		// cup 2
 
-	while (1);
+		sleep (5);
+		
+		pwmWrite (PWM_PIN, 220);		// cup 1
+		
+		sleep (5);
+		
+		pwnWrite (PWM_PIN, 90);			// cup 3
+		
+		sleep (5);
+	}
 	
 	return 0;
 }
