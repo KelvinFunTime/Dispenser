2c3325e48f0d064ed4c7e3ecb1c0a5e803c8b8b6
KelvinFunTime <kelvinfuntime@gmail.com>
2013-05-18 08:23:34 +0000

    100% functional. Bug checking is a high priority now.

--- build_release/moc_TimerLevel.cpp
+++ build_release/moc_TimerLevel.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'TimerLevel.h'
 **
-** Created: Sun Apr 28 01:09:05 2013
+** Created: Sat May 18 07:16:22 2013
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.2)
 **
 ** WARNING! All changes made in this file will be lost!
